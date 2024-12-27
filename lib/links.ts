1bcb9ff36920dc3025b6d6f47df013b10d9967db
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2024-12-27 11:04:09 +0800

    new linkedin link

--- lib/links.ts
+++ lib/links.ts
@@ -15,7 +15,8 @@ export const NavBarLinks = [
 
 export enum ProjectURLs {
     OwnGithub = "https://github.com/icecubes99",
-    OwnLinkedIn = 'https://www.linkedin.com/in/emmanuel-aguado-0a17162ab/',
+    // OwnLinkedIn = 'https://www.linkedin.com/in/emmanuel-aguado-0a17162ab/',
+    OwnLinkedIn = 'https://www.linkedin.com/in/EmmanuelAguado/',
     OwnBehance = 'https://www.behance.net/emmanueaguado',
     OwnResume = "https://drive.google.com/file/d/1K68dDTDnBCkrC_KQ6NM3I9L7Z4Mdvu9L/view?usp=sharing",
     THAPBehance = 'https://www.behance.net/gallery/215005673/The-Happy-Asian-Pessimist',
