d505a2498af34b1b72cfd6941b06003b214c2148
Emmanuel Aguado <aguadojr.emmanueldiaz@gmail.com>
2026-05-26 19:43:13 +0800

    feat: new resume

--- lib/links.ts
+++ lib/links.ts
@@ -21,7 +21,7 @@ export enum ProjectURLs {
     OwnGithub = "https://github.com/icecubes99",
     OwnLinkedIn = "https://www.linkedin.com/in/EmmanuelAguado/",
     OwnBehance = "https://www.behance.net/emmanueaguado",
-    OwnResume = "https://drive.google.com/file/d/1eW4-L3K_4hhGxSbKHzrKQu_-ZakzxuJv/view?usp=sharing",
+    OwnResume = "https://drive.google.com/file/d/1QhlUOjgZh03LRiY-_PCaMZm-iNPFYXUZ/view?usp=sharing",
     OwnEmail = "mailto:aguado@emman@gmail.com",
     OwnLetterboxd = "https://letterboxd.com/icecubes99/",
     THAPBehance = "https://www.behance.net/gallery/215005673/The-Happy-Asian-Pessimist",
